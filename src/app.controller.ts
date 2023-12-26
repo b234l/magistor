ab3f9ce59a58687425e8cd79ab9eb02b07c2d497
b234l <korolevaleksej12@gmail.com>
2023-12-27 00:14:07 +0300

    fix: импорт встроенных методов ; использование методов Req и Resp для реализации вопроса и ответа сервера ; проверка выполнена при помощи Postman

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  getHello(
+    @Req() req: Request,
+    @Res() res: Response
+  ) {
+    console.log(req.query);
+
+    return res.status(200).send({
+      status: 'All right'
+    })
   }
 }
